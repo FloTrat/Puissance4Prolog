b13eb73cf2d7a93c82f51544c6b48dd929bbb43e
“Jules <jules.sanglier@insa-lyon.fr>
2022-11-07 20:50:17 +0100

    🚧 - tests : kmoy

--- SIGSWAGPuissance4/testIAs.pl
+++ SIGSWAGPuissance4/testIAs.pl
@@ -22,6 +22,7 @@
 % NbIterations: le nombre de parties à jouer,
 % IA1 et IA2 : les identifiants des 2 IA à confronter
 % IA1 joue contre IA2 "NbIterations" fois le predicat affiche combien de fois qui a battu qui
+% On cherche à évaluer le nbr de coups moyen qu'il a fallu pour gagner
 runTest(NbIterations,IA1,IA2) :-
 	NbIterationsParIA is NbIterations//2,
 	call_time(runTestIAXEnPremier(NbIterationsParIA,IA1,IA2,0,NbFoisIA1GagneEnCommencant,0,NbFoisIA1PerdEnCommencant), Dict1),
@@ -31,7 +32,7 @@ runTest(NbIterations,IA1,IA2) :-
 	assert(STATS_IA1(Dict1.get(wall), Dict1.get(inferences), Dict2.get(wall), Dict2.get(inferences))),
 	% STATS_IA1 = {TEMPS_SI_IA1_COMMENCE, NB_ITERATIONS_SI_IA1_COMMENCE, TEMPS_SI_IA1_NE_COMMENCE_PAS, NB_ITERATIONS_SI_IA1_NE_COMMENCE_PAS}
 
-	assert(STATS_IA2(Dict2.get(wall), Dict2.get(inferences), STATS_IA1(Dict2.get(wall), Dict1.get(inferences))),
+	assert(STATS_IA2(Dict2.get(wall), Dict2.get(inferences), STATS_IA1(Dict2.get(wall), Dict1.get(inferences)))),
 	% STATS_IA2 = {TEMPS_SI_IA2_COMMENCE, NB_ITERATIONS_SI_IA2_COMMENCE, TEMPS_SI_IA2_NE_COMMENCE_PAS, NB_ITERATIONS_SI_IA2_NE_COMMENCE_PAS}
 	write(TypeIA2), write(' en commençant : a gagné '), write(NbFoisIA2GagneEnCommencant),write(' fois et a perdu '),write(NbFoisIA2PerdEnCommencant),write(' fois.'),
 	nl,
@@ -53,17 +54,49 @@ benchmark(NbIterations, IA1, IA2) :-
 %% Prédicats privés %%
 %%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Gestion Data Benchmark %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+saveNbRuns(X) :- assert(testRuns(X)).
+
+resetCoupsIAs :- retract(ia1Coups(X)), retract(ia2Coups(Y)), assert(ia1Coups(0)), assert(ia2Coups(0)).
+
+%resetTimerIAs :- retract(ia1Timer(X)), retract(ia2Timer(Y)), assert()
+
+incrementCoupsIA(IA) :-
+ joueurCourant(X, IA),
+ X = rouge,
+ 	ia1Coups(S), E is S+1, retract(ia1Coups(S)), assert(ia1Coups(E));
+ X = jaune,
+ 	ia2Coups(S), E is S+1, retract(ia2Coups(S)), assert(ia2Coups(E)).
+
+% temps moyen avant de gagner
+%calculTmpMoy(IA, NbIterations) :-
+
+
+calculKMoy(IA, NbIterations) :- 
+ joueurCourant(X, IA),
+ X = rouge, ia1Coups(S), write('KMoy pour '), write(IA), M is S / NbIterations, write(M);
+ X = jaune, ia2Coups(S), write('KMoy pour '), write(IA), M is S / NbIterations, write(M).
+
 % test de sortie de runTestIAXEnPremier
-runTestIAXEnPremier(0,_,_,NbIA1GagneIni,NbIA1GagneIni,NbIA2GagneIni,NbIA2GagneIni) :- !.
+runTestIAXEnPremier(0,_,_,NbIA1GagneIni,NbIA1GagneIni,NBNbIA2GagneIni,NbIA2GagneIni) :-
+    calculKMoy(IA1, testRuns(X)),
+	calculKMoy(IA2, testRuns(X)),
+	!.
+
 runTestIAXEnPremier(NbIterations,IA1,IA2,NbIA1GagneIni,NbIA1GagneFin,NbIA2GagneIni,NbIA2GagneFin) :-
+	saveNbRuns(NbIterations), % va être lancé à chaque fois mais ne sera assert que la première fois
 	init,
+	resetCoupsIAs,
 	assert(joueurCourant(rouge,IA1)), % rouge = IA1
 	assert(autreJoueur(jaune,IA2)),
 	jeu(PartieNulle),
 	joueurCourant(CouleurIAGagnante,_),
 	incrementerGagnant(PartieNulle,CouleurIAGagnante,NbIA1GagneIni,NbIA1GagneFin1,NbIA2GagneIni,NbIA2GagneFin1),
 	NbIterations2 is NbIterations-1,
-	runTestIAXEnPremier(NbIterations2,IA1,IA2,NbIA1GagneFin1,NbIA1GagneFin,NbIA2GagneFin1,NbIA2GagneFin).
+	runTestIAXEnPremier(NbIterations2,IA1,IA2,NbIA1GagneFin1,NbIA1GagneFin,NbCoupsIA1,NbIA2GagneFin1,NbIA2GagneFin,NbCoupsIA2).
 
 incrementerGagnant(true,_,NbIA1GagneIni,NbIA1GagneIni,NbIA2GagneIni,NbIA2GagneIni).
 incrementerGagnant(false,rouge,NbIA1GagneIni,NbIA1GagneFin,NbIA2GagneIni,NbIA2GagneIni) :-
@@ -78,6 +111,7 @@ tour(PartieNulle) :-
 	joueurCourant(CouleurJCourant,TypeJoueur),
 	obtenirCoup(CouleurJCourant,TypeJoueur,Coup),
 	placerJeton(Coup,Y,CouleurJCourant),
+	incrementCoupsIA(TypeJoueur),
 	testFin(Coup,Y,CouleurJCourant, PartieNulle).
 
 testFin(Coup,Y,CouleurJCourant,PartieNulle) :-
